d94f5776c7854d08481668eac8352c6a23ba2cdf
arozumenko <artyom.rozumenko@gmail.com>
2022-09-06 09:08:42 +0300

    wrapped options with div

--- options.js
+++ options.js
@@ -5,16 +5,20 @@ class optionsMenu {
     id;
     config;
     htmlTemplate = `
-    <a class="btn btn-sm btn-secondary btn-block mb-0" data-toggle="collapse" href="#{{modal.id}}_{{ms.no}}" role="button" aria-expanded="false" aria-controls="{{modal.id}}_{{ms.no}}">
-    {{if(options.ms.name)}} {{ms.name}} {{#else}} Options {{/if}}
-    <div class="ripple-container"></div>
-    </a>
-    <div class="collapse" id="{{modal.id}}_{{ms.no}}">
-      <div class="card card-body card-dark">
-        <div>
-          {{each(options.ms.content)}}
-            {{ @this.content | safe }}
-          {{/each}}
+    <div>
+      <a class="btn btn-sm btn-secondary btn-block mb-0" 
+        data-toggle="collapse" href="#{{modal.id}}_{{ms.no}}" 
+        role="button" aria-expanded="false" aria-controls="{{modal.id}}_{{ms.no}}">
+      {{if(options.ms.name)}} {{ms.name}} {{#else}} Options {{/if}}
+      <div class="ripple-container"></div>
+      </a>
+      <div class="collapse" id="{{modal.id}}_{{ms.no}}">
+        <div class="card card-body card-dark">
+          <div>
+            {{each(options.ms.content)}}
+              {{ @this.content | safe }}
+            {{/each}}
+          </div>
         </div>
       </div>
     </div>`
