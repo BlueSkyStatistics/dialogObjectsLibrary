11e2260eb4c479b79e0a9a791adbde95ed4892f0
Aspect13 <ad13box@gmail.com>
2024-07-02 20:52:31 +0400

    Update settingsOptions.js

--- settingsOptions.js
+++ settingsOptions.js
@@ -126,6 +126,10 @@ class MiscOpt {
         <label class="form-check-label" for="hideOutputSaveWarning">Hide output save warning on application
           exit</label>
       </div>
+       <div id="hidePasteWarningDiv" class="form-check pb-3">
+        <input class="form-check-input" type="checkbox" id="hidePasteWarning" name="hidePasteWarning">
+        <label class="form-check-label" for="hidePasteWarning">Hide warning when pasting overflowing data</label>
+      </div>
       <div id="useLocalFileDiv" class="form-check pb-3">
         <input class="form-check-input" type="checkbox" id="useLocalFile"
           name="useLocalFile">
@@ -156,12 +160,24 @@ class MiscOpt {
       </div>  
       <div id="factorCreationControlDiv" class="form-check pb-3">
         <input class="form-check-input" type="checkbox" id="factorCreationControl" name="factorCreationControl" onclick="maxFactorCheckedChange(this)">
-        <label class="form-check-label" for="factorCreationControlDiv">Control the creation of factor variables</label>
+        <label class="form-check-label" for="factorCreationControlDiv">Convert character variables to factor when importing datasets</label>
+        <label>
+        For the files of type Excel, CSV, DAT and SAS we automatically convert all the character variables to factor. For all other file types, we rely on the R package to set the class of the dataset variables. This setting post processes the dataset after opening it to allow you to control the creation of factor variables based on the criteria below.
+        </label>
         <div id="maxfactorcountdiv" class="pb-3">
-          <label class="form-check-label ml-4 w-50" for="maxfactorcount">Specify the maximum number of distinct values to control creation of factor variables</label>
+          <label class="form-check-label ml-4 w-50" for="maxfactorcount">Specify the maximum number of distinct values to control creation of factor variables. When the number of unique values in a variable exceeds the speicified value we will create a variable of class character else a variable of class factor is created.</label>
           <input class="w-25 float-right" type="number" id="maxfactorcount" name="maxfactorcount">
         </div>
-      </div>              
+        <label>
+          1: Uncheck this setting to load character variables as character. 
+        </label>
+        <label>
+          2: Check this setting and provide a positive number to create a factor if the variable contains unique values less than or equal the specified number. 
+        </label>
+        <label>
+          3: Check this setting and set a value of 0 to convert all character variables to factor.
+        </label>                
+      </div>   
     `
     constructor(modal, config) {
         this.content = Sqrl.Render(this.htmlTemplate, { modal: modal, ms: config })
@@ -170,6 +186,56 @@ class MiscOpt {
         return true
     }
     clearContent() { }
+}
+
+
+class RLocaleOpt {
+  content;
+  id; 
+  htmlTemplate = `
+                <div id="rlocalediv" class="pb-3">
+                <label class="form-check-label mr-2" for="rlocale">Set R locale :</label>
+                <input list="rlocalelist" name="rlocale" class="w-25 float-right" id="rlocale">
+                <datalist id="rlocalelist">
+                  <option value="Chinese (Simplified)">
+                  <option value="Turkish">
+                  <option value="French">
+                  <option value="Portuguese">
+                </datalist>                
+                  <div id="rlocalekiddiv" class="pb-3">
+                  <p>
+                  <label class="form-check-label mr-2">1. If you want to set to the system default locale, manually delete any text in the field if it is not already empty.</label>
+                  <p>
+                  <label class="form-check-label mr-2">The following examples are illustration purposes only to explain what system default locale setting means  </label>
+                  <p>
+                  <label class="form-check-label mr-2">
+                  Example 1: if you purchased and configured a PC in the USA, your system defaut locale will likely be something like "English_United States".
+                  </label>
+                  <p>
+                  <label class="form-check-label mr-2">
+                  Example 2: if you purchased and configured a PC in China, your system default locale will likely be something like "Chinese (Simplified)".
+                  </label>
+                  <p>
+                  <label class="form-check-label mr-2">2. To pick a locale from the dropdown, first make the text field empty by manully deleting any text. Click on the text field to get the dropdown to appear with multiple choices. If you do not find a suitable locale choice in the dropdown, you can hand type a valid locale in the text field.</label>
+                  <p>
+                  <label class="form-check-label mr-2">3. Whenever you want to change the locale and select from the dropdown, you must first manually delete the current text entry to make the text field empty, and then you will see the dropdown to pick a value from.</label>
+                  <p>
+                  <label class="form-check-label mr-2">Note: If the graphs/plots are not displaying the legends correctly in the correct locales, you must also go to the Theme dialog (see toolbar above) and select a suitable font from the dropdown list. </label>
+                  <p>
+                  <label class="form-check-label mr-2">
+                  Example : if you set locale to "Chinese (Simplified)", you should also set the font to "Microsoft Yahei" or "SimSun" in the Theme dialog</label>
+                  <p>
+                  <label class="form-check-label mr-2">visit the support page on our website (www.blueskystatistics.com) for the related technote and further deatils</label>
+                  </div>            
+                </div>
+  `
+  constructor(modal, config) {
+      this.content = Sqrl.Render(this.htmlTemplate, { modal: modal, ms: config })
+  }
+  canExecute() {
+      return true
+  }
+  clearContent() { }
 }
 
 class DatabaseOpt {
@@ -213,5 +279,6 @@ class SaveAppSettings {
 module.exports.OutputOpt = OutputOpt;
 module.exports.OutputTblOpt = OutputTblOpt;
 module.exports.MiscOpt = MiscOpt;
+module.exports.RLocaleOpt = RLocaleOpt;
 module.exports.DatabaseOpt = DatabaseOpt;
 module.exports.SaveAppSettings = SaveAppSettings;
